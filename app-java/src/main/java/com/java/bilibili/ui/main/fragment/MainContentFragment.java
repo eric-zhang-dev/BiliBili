5031250a3d4f01fd290a6767132d353c9f67ca78
zhangyue <eric.zhang.dev@gmail.com>
2018-04-04 18:24:00 +0800

    some files

--- app-java/src/main/java/com/java/bilibili/ui/main/fragment/MainContentFragment.java
+++ app-java/src/main/java/com/java/bilibili/ui/main/fragment/MainContentFragment.java
@@ -1,10 +1,15 @@
 package com.java.bilibili.ui.main.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 
 import com.java.bilibili.R;
 import com.java.bilibili.base.RxBaseFragment;
@@ -12,6 +17,7 @@ import com.java.bilibili.ui.main.fragment.adapter.HomePagerAdapter;
 import com.java.bilibili.ui.main.fragment.impl.MainImpl;
 import com.java.bilibili.ui.main.fragment.presenter.MainPresenter;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,16 +33,21 @@ public class MainContentFragment extends RxBaseFragment<MainPresenter> implement
     ViewPager viewPager;
     @BindView(R.id.tabs)
     TabLayout tabs;
+    @BindView(R.id.bottom_navi_view)
+    BottomNavigationView bottomNavigationView;
     HomePagerAdapter mHomePagerAdapter;
     private List<Fragment> mFragments;
+
     @Override
     public void showLoading() {
 
     }
+
     public static MainContentFragment newInstance() {
         MainContentFragment fragment = new MainContentFragment();
         return fragment;
     }
+
     @Override
     public void showError(String msg) {
 
@@ -59,6 +70,7 @@ public class MainContentFragment extends RxBaseFragment<MainPresenter> implement
 
     @Override
     protected void initData() {
+        disableShiftMode(bottomNavigationView);
         FragmentManager fm = getChildFragmentManager();
         mFragments = new ArrayList<>();
 
@@ -76,5 +88,29 @@ public class MainContentFragment extends RxBaseFragment<MainPresenter> implement
         tabs.setupWithViewPager(viewPager);
 
     }
-    String[] t = {"A","B","C","D","E","F","G"};
+
+    String[] t = {"A", "B", "C", "D", "E", "F", "G"};
+
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
 }
---
+++ app-java/src/main/java/com/java/bilibili/ui/play/PlayActivity.java
@@ -0,0 +1,4 @@
+package com.java.bilibili.ui.play;
+
+public class PlayActivity {
+}
